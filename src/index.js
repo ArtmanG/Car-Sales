8fdba2c20af7a8dad32b0cd6655d003165485c83
Gregory Hawman <g.hawman@gmail.com>
2020-03-18 11:00:43 -0700

    mapStateToProps added everywhere needed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import App from './App';
 import { appReducer } from './reducers/appReducer.js';
 
 import 'bulma/css/bulma.css';
