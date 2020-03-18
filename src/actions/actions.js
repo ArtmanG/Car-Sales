9259563c52f6cd9c2149a8385b95c91c1b65acec
Gregory Hawman <g.hawman@gmail.com>
2020-03-18 12:43:20 -0700

    moved my actions from App.js over to actions.js

---
+++ src/actions/actions.js
@@ -0,0 +1,10 @@
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const ADD_ITEM = 'ADD_ITEM';
+
+const removeFeature = item => {
+    // dispatch an action here to remove an item
+  };
+
+  const buyItem = item => {
+    // dipsatch an action here to add an item
+  };
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import App from './App';
 import { appReducer } from './reducers/appReducer.js';
-
+import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
